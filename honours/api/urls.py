e3ea323faed6708f0cc0b77a3ec112fcf6d7ff35
Timothy Oliver <oliverwakogi@live.com>
2019-11-25 12:21:50 +0300

    create views for each model

--- honours/api/urls.py
+++ honours/api/urls.py
@@ -1,7 +1,18 @@
+from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
+from . import views
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'profiles', views.UserProfileViewSet)
+router.register(r'projects', views.ProjectViewSet)
+router.register(r'projectRatings', views.ProjectRatingViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- honours/api/views.py
+++ honours/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, UserProfileSerializer, ProjectSerializer, ProjectRatingSerializer
+from .models import UserProfile, Project, ProjectRating
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    # def list(self, request, *args, **kwargs):
+    #     users = User.objects.all()
+    #     serializer = self.get_serializer(users, many=True)
+    #     return Response(serializer.data)
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ProjectRatingViewSet(viewsets.ModelViewSet):
+    queryset = ProjectRating.objects.all()
+    serializer_class = ProjectRatingSerializer
