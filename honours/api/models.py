f60f927792919bad0f12ad5b267ea32b68db6352
Timothy Oliver <oliverwakogi@live.com>
2019-11-25 10:01:45 +0300

    fix image and relationship bugs

--- honours/api/models.py
+++ honours/api/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    profile_pic = models.ImageField(upload_to='/profile-images', default='images/default.jpg')
+    profile_pic = models.ImageField(upload_to='profile-images', default='images/default.jpg')
     bio = models.TextField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE())
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Project(models.Model):
@@ -13,13 +13,13 @@ class Project(models.Model):
     landing_page_image = models.ImageField(upload_to='profile-images', default='images/default.jpg')
     description = models.TextField()
     link = models.CharField(max_length=255)
-    user = models.ForeignKey(User, on_delete=models.CASCADE())
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
 
 
 class ProjectRating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE())
-    project = models.ForeignKey(User, on_delete=models.CASCADE())
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rater')
+    project = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project_rated')
     design = models.IntegerField()
     usability = models.IntegerField()
     content = models.IntegerField()
