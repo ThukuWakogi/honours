72854aabc1f0e4c692f342d68def2af532680dc1
Timothy Oliver <oliverwakogi@live.com>
2019-11-25 09:30:45 +0300

    create models

--- honours/api/models.py
+++ honours/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    profile_pic = models.ImageField(upload_to='/profile-images', default='images/default.jpg')
+    bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE())
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    landing_page_image = models.ImageField(upload_to='profile-images', default='images/default.jpg')
+    description = models.TextField()
+    link = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE())
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ProjectRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE())
+    project = models.ForeignKey(User, on_delete=models.CASCADE())
+    design = models.IntegerField()
+    usability = models.IntegerField()
+    content = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
