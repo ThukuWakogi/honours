744ecdf6c7f3f7bb5199862bf6c50efebe8423f5
Timothy Oliver <oliverwakogi@live.com>
2019-11-28 17:33:44 +0300

    change settings

--- honours/settings.py
+++ honours/settings.py
@@ -137,4 +137,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals()) 
